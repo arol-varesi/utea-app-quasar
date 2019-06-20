2172cd743bc340868c39739e7d7bb9ed53f779f7
alv67 <alv67.web@gmail.com>
2019-06-20 22:41:03 +0200

    Prima di aggiungere SQLite e TypeOrm

--- quasar.conf.js
+++ quasar.conf.js
@@ -42,7 +42,8 @@ module.exports = function (ctx) {
         'QItemSection',
         'QItemLabel',
         'QCard',
-        'QCardSection'
+        'QCardSection',
+        'QTable'
       ],
 
       directives: [
